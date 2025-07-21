b329d633e5d593853ae54a3bb88a001637cdce3c
Nazmul Hoque ovy <55842205+ovibinzia1885@users.noreply.github.com>
2025-07-21 17:45:04 +0600

    Asseration

---
+++ tests/Asseration.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+test('Asseration', async ({ page }) => {
+    await page.goto('https://demowebshop.tricentis.com/register');
+
+    //expect(page).toHaveURL()	Page has URL
+    await expect(page).toHaveURL('https://demowebshop.tricentis.com/register')
+    //expect(page).toHaveTitle()	Page has title
+    await expect(page).toHaveTitle('Demo Web Shop. Register');
+
+   // expect(locator).toBeVisible()	Element is visible
+   const logo=await page.locator('.header-logo');
+   await expect(logo).toBeVisible();
+   //expect(locator).toBeEnabled()	Control is enabled
+   const SearchBox= await page.locator('#small-searchterms');
+   await expect(SearchBox).toBeEnabled();
+
+  // expect(locator).toBeChecked()	Checkbox is checked
+  const RadioButton=await page.locator('#gender-male');
+  RadioButton.click();
+  await expect(RadioButton).toBeChecked()
+
+  //expect(locator).toHaveAttribute()	Element has attribute
+
+  const attBute= await page.locator('#register-button')
+  await expect(attBute).toHaveAttribute('type','submit')
+
+})
